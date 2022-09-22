4b86a978583e3378815848ccdf8fd1a0a3b616c8
Daiki48 <daiki48.engineer@gmail.com>
2022-09-23 00:30:56 +0900

    [add] textarea tag check

--- src/components/entry.rs
+++ src/components/entry.rs
@@ -3,22 +3,36 @@ use dioxus::prelude::*;
 #[allow(non_snake_case)]
 pub fn Entry(cx: Scope) -> Element {
     let item = use_state(&cx, || "".to_string());
+    let article = use_state(&cx, || "".to_string());
 
     cx.render(rsx!(
         style { [include_str!("../../static/entry.scss")] }
         h1 {
             "This page is Entry"
         }
-        input {
-            oninput: move |evt| {
-                item.set(evt.value.clone())
+        div {
+            class: "itemarea",
+            input {
+                placeholder: "This is item",
+                oninput: move |evt| {
+                    item.set(evt.value.clone())
+                }
+            }
+            br {}
+            label {
+                "{item}"
             }
         }
-        label {
-            "item is {item}"
-        }
-
         textarea {
+            rows: "10",
+            cols: "60",
+            placeholder: "This is article",
+            oninput: move |e| {
+                article.set(e.value.clone())
+            }
+        }
+        p {
+            "{article}"
         }
 
     ))
