a3e1822b5b3e9a2dcd5fe6b23da0a29f583817cd
Daiki48 <daiki48.engineer@gmail.com>
2022-09-23 20:29:03 +0900

    [add] Description for this website

--- src/components/home.rs
+++ src/components/home.rs
@@ -1,11 +1,18 @@
 use dioxus::prelude::*;
 
+
 #[allow(non_snake_case)]
 pub fn Home(cx: Scope) -> Element {
     cx.render(rsx!(
         style { [include_str!("../../static/home.scss")] }
         h1 {
             "This page is Home"
+        }
+        p {
+            "このサイトは、私のdioxus勉強用サイトです。使える機能を試すためのものです。ソースコードもgithubから確認出来ます。"
+        }
+        p {
+            "This is my dioxus study site. It is for testing the features available. Source code is also available from github."
         }
     ))
 }
